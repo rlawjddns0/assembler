7f87ac9d8d031da49b6244e2926990952a3ae0ae
kjw <rlawjddns0@naver.com>
2018-11-12 21:13:59 +0900

    2nd

--- check.c
+++ check.c
@@ -27,26 +27,21 @@ int is_valid(char *op, char *args)
 	}
 	
 	index=i;
-	if(args[0]=='%'&&args[index+1]=='%')
-		return 1;
-	else if(args[0]=='('&&args[1]=='%'&&args[index+1]=='%')
-		return 1;
-	else if(args[0]=='-'){
-		if(args[1]=='0'&&args[2]=='x'&&args[index+1]=='%'){
-			return 1;
-		}
+	if(args[0]=='-' || args[0]=='0' || args[0]=='(')
+	{
+		if(args[index+1]=='0')
+			return 0;
+		else if(args[index+1]=='(')
+			return 0;
 	}
-	else if(args[0]=='0'&&args[1]=='x'&&args[index+1]=='%'){
-		return 1;
-	}
-	else if(args[0]=='%' &&args[index+1]=='0'&&&&args[index+2]=='x'){
-		return 1;
-	}
-	else if(args[0]=='$'&&args[1]=='0'&&args[2]=='x'&&args[index+1]=='%'){
-		return 1;
-	}
-	else
+	if(args[0]='$' && args[index+1]=='$')
 		return 0;
 
+	if(args[index+1]=='$')
+		return 0;
+
+
+return 1;
+}	
 			
 		
