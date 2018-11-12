a20f196ca7d9e55a578303c036534ea79498a1e3
kjw <rlawjddns0@naver.com>
2018-11-12 20:42:41 +0900

    Edit check.c

--- check.c
+++ check.c
@@ -8,12 +8,45 @@ int is_valid(char *op, char *args)
 	/********************************/
     /*  							*/
 	/*         syntax check         */
-    /*  			adsfasdfasdfdsf
-asdfasdfasdfasd
-fadsfadsfasdfasdfasdf
-asdfasdfasdfasdf
+    /* 
+
 				*/
 	/********************************/
+
+	int i=0;
+	int index;
+
+	if(strcmp(op,"mov")!=0)
+	{
+		return 0;
+	}
+	for(i=0; i<strlen(args); i++)
+	{
+		if(str[i]=='.')
+			break;
+	}
 	
-	return 1;
-}
+	index=i;
+	if(args[0]=='%'&&args[index+1]=='%')
+		return 1;
+	else if(args[0]=='('&&args[1]=='%'&&args[index+1]=='%')
+		return 1;
+	else if(args[0]=='-'){
+		if(args[1]=='0'&&args[2]=='x'&&args[index+1]=='%'){
+			return 1;
+		}
+	}
+	else if(args[0]=='0'&&args[1]=='x'&&args[index+1]=='%'){
+		return 1;
+	}
+	else if(args[0]=='%' &&args[index+1]=='0'&&&&args[index+2]=='x'){
+		return 1;
+	}
+	else if(args[0]=='$'&&args[1]=='0'&&args[2]=='x'&&args[index+1]=='%'){
+		return 1;
+	}
+	else
+		return 0;
+
+			
+		
