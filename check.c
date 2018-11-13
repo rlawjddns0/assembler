9608bb6bba792a24effce04ecce9fbf7171d8441
kjw <rlawjddns0@naver.com>
2018-11-13 13:53:07 +0900

    Result

--- check.c
+++ check.c
@@ -1,18 +1,6 @@
 #include "myas.h"
 
-int is_valid(char *op, char *args)
-{
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /* 
-
-				*/
-	/********************************/
-
+int is_valid(char *op, char *args){
 	int i=0;
 	int index;
 
--- trans.c
+++ trans.c
@@ -9,16 +9,7 @@ int instr_trans(char *op, char *args, char* mcode)
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
-
-
 	strcpy(mcode,"AB CD EF");	
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
-
-
 
 	for(i=0; args[i]!=NULL;i++){
 		if(args[i]==',')
@@ -32,7 +23,7 @@ int instr_trans(char *op, char *args, char* mcode)
 		strcpy(mcode,"89");
 	}//reg to reg (1)
 
-	if(args[0]=='%' &&args[index]=='0'&&args[index+1]=='x')
+	if(args[0]=='%' &&args[index]=='0'&&args[index+1]=='x'||args[index]=='(')
 		strcpy(mcode,"a3");//reg(eax) to mem (5)
 	
 
@@ -55,24 +46,15 @@ int instr_trans(char *op, char *args, char* mcode)
 		}
 	}//immediate to reg(6)
 
-
-
-	if(args[index]=='%'&&args[index+1]=='e' &&args[index+2]=='a')
+	if(args[index]=='%'&&args[index+1]=='e')
 	{
-		if(args[0]=='0')
+		if(args[0]=='0'&&args[index+2]=='a')
 			strcpy(mcode,"a1");
 		else if(args[index]=='%'){
 			if(args[index-2]==')')
 				strcpy(mcode,"8b");
-
 		}
 	}
-
-
-
-	
-
-	
 	
 
 	return 1;	
